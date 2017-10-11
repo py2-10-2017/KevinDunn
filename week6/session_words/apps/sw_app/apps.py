b8f83ce020005d03ab6b5c01eee7680c4585bfaf
kevind <kevincdunn.kcd@gmail.com>
2017-10-11 14:15:54 -0500

    session words assignment

---
+++ week6/session_words/apps/sw_app/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class SwAppConfig(AppConfig):
+    name = 'sw_app'
---
+++ week6/session_words/apps/sw_app/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, HttpResponse, redirect
+from datetime import datetime
+
+# Create your views here.
+def index(request): 
+	try: 
+		request.session["added_words"]
+	except KeyError: 
+		request.session["added_words"] = []
+
+	return render(request, 'sw_app/index.html')
+
+
+def add_word(request):
+	add_word = {}
+	for key, value in request.POST.iteritems():
+		if key != "csrfmiddlewaretoken" and key != "big":
+			add_word[key] = value 
+		if key == "big":
+			add_word["class"] = "big"
+		else:
+			add_word["class"] = "none"
+			
+	add_word["the_word"] = request.POST["the_word"]
+	add_word["created_at"] = datetime.now().strftime("%H:%M %p, %B %d, %Y")
+	
+	temp = request.session["added_words"]
+	temp.append(add_word)
+	request.session["added_words"] = temp
+
+	# for key, val in request.session.__dict__.iteritems():
+	# 	print "the key and value are: ", key, val
+	# print "post created at", add_word
+	return redirect('/')
+
+
+def result(request):
+	return redirect('/')
+
+def reset(request):
+	for key in request.session.keys():
+		del request.session[key]
+	return redirect('/')
