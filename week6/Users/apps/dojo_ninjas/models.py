4b4d3d2ef3749b1c133deae6aa3edfdeec436225
kevind <kevincdunn.kcd@gmail.com>
2017-10-16 11:10:08 -0500

    added dojo model

--- week6/Users/apps/dojo_ninjas/models.py
+++ week6/Users/apps/dojo_ninjas/models.py
@@ -3,3 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Dojo(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.Charfield(max_lengh=255)
+    state = models.Charfield(max_lengh=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+         return "<Blog object: {} {} {}>".format(self.name, self.city, self.state)
+
