671edb151e2354559d65f58ccaf62a4794b4f33c
kevind <kevincdunn.kcd@gmail.com>
2017-10-16 17:40:57 -0500

    book_auhors assigment

--- week6/Users/apps/book_authors/models.py
+++ week6/Users/apps/book_authors/models.py
@@ -9,10 +9,17 @@ class Book(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+         return "<Book object: {}>".format(self.name)
+
 class Author(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
+    notes = models.TextField(default='')
     books = models.ManyToManyField(Book, related_name="authors")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+         return "<Author object: {} {}>".format(self.first_name, self.last_name)
