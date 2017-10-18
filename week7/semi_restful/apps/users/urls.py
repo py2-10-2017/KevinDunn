2fd26ed531503f3f56b276f0903396ea3abf5228
kevind <kevincdunn.kcd@gmail.com>
2017-10-18 14:27:20 -0500

    removed unneeded urlpattern

--- week7/semi_restful/apps/users/urls.py
+++ week7/semi_restful/apps/users/urls.py
@@ -8,8 +8,6 @@ urlpatterns = [
     url(r'^users/create$', views.create),
     url(r'^users/(?P<id>\d+)$', views.show),
     url(r'^users/(?P<id>\d+)/edit/$', views.edit),
-    url(r'^users/update$', views.update),
-    url(r'^users/(?P<id>\d+)/update$', views.update),  #try this
-    url(r'^users/(?P<id>\d+)/update/$', views.update),
+    url(r'^users/(?P<id>\d+)/update$', views.update),
     url(r'^users/(?P<id>\d+)/delete/$', views.destroy)
 ]
